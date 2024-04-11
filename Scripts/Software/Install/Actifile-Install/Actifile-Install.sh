b3a3e63e7cab7ec6f0e7d8e561a0f1ad2a9822ab
Drew McClellan <d.mcclellan013@gmail.com>
2024-04-11 14:50:14 -0400

    AF update - 2

--- Scripts/Software/Install/Actifile-Install/Actifile-Install.sh
+++ Scripts/Software/Install/Actifile-Install/Actifile-Install.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
-## Notes
-#### The curl URL below can be found on client sites.
+
 # Installs Actifile if it is not already.
-if which /Library/Application Support/Actifile >/dev/null; then
-    #echo "Actifile is already installed...ending script"
+Install_Path="/Library/Application Support/Actifile"
+
+if [ -d "$Install_Path" ]; then
+    echo "Actifile is already installed...ending script"
 else
-    # Program URL Var.
+    # Program DL URL Var.
     DOWNLOAD_URL1="https://support.actifile.com/wp-content/uploads/2021/06/silent_install.zip"
-    # download .zip unzipped name.
+    # .zip unzipped name.
     SLT_Installer_Zip="silent_install.zip"
     # Temp dir location full path.
     Temp_Location="/var"
+
     echo "Actifile is not installed...installing Actifile"
-    #Set the dir so mktemp will be made
+    # Changes the dir location to the temp location
     cd "$Temp_Location"
-    # Creates a temporary directory.
+    # Makes a folder in the temp location
     mktemp -d -t temp99
-    # Downloads the silent installer .zip to the tmp dir.
-    curl -sSL "$DOWNLOAD_URL1" -O "$Temp_Location"
-    # Unzips the silent installer
-    unzip $SLT_Installer_Zip -d "$Temp_Location"
-    # Makes the script executable
+    # Downloads the file from the URL var above.
+    curl -sSL "$DOWNLOAD_URL1" -o "$Temp_Location/$SLT_Installer_Zip"
+    # unzips the DL.
+    unzip "$Temp_Location/$SLT_Installer_Zip" -d "$Temp_Location"
+    # Makes the DL script executable.
     chmod +x install_actifile.sh
     # Silently installs the program. Be sure to change the install key!!!
     ./silent_install.sh install INSTALL_KEY
-    # Checks to see if RC installed or not.
-    if which /Library/Application Support/Actifile >/dev/null; then
-        echo "Actifile install successful!"#
+
+    # Checks to see if Actifile installed or not.
+    if [ -d "$Install_Path" ]; then
+        echo "Actifile install successful!!!"
     fi
 fi
 
