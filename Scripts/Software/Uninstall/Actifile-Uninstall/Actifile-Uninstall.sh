87f572883087877b5894541e1c4dcbc97cd0c73c
Drew McClellan <d.mcclellan013@gmail.com>
2024-04-11 15:36:14 -0400

    AF uninstall and install update - 1

--- Scripts/Software/Uninstall/Actifile-Uninstall/Actifile-Uninstall.sh
+++ Scripts/Software/Uninstall/Actifile-Uninstall/Actifile-Uninstall.sh
@@ -4,7 +4,7 @@
 Install_Path="/Library/Application Support/Actifile"
 
 if [ -d "$Install_Path" ]; then
-    echo "Actifile is already installed...ending script."
+    echo "Actifile is already uninstalled...ending script."
 else
     # Program DL URL Var.
     DOWNLOAD_URL1="https://support.actifile.com/wp-content/uploads/2021/06/silent_install.zip"
@@ -24,12 +24,14 @@ else
     unzip "$Temp_Location/$SLT_Installer_Zip" -d "$Temp_Location"
     # Makes the DL script executable.
     chmod +x install_actifile.sh
-    # Silently installs the program. Be sure to change the install key!!!
-    ./silent_install.sh install "INSTALL_KEY"
+    # Silently uninstalls the program. Be sure to specify the user to uninstall the program
+    sudo ./ActifileMacAgentInstaller.app/Contents/MacOS/FileControlInstaller uninstall --user root
 
+    # waits for the uninstall to finish
+    sleep 180
     # Checks to see if Actifile installed or not.
-    if [ -d "$Install_Path" ]; then
-        echo "Actifile install successful!!!"
+    if [ ! -d "$Install_Path" ]; then
+        echo "Actifile uninstall successful!!!"
     fi
 fi
 
